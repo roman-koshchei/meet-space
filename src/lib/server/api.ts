769d3fadad599ea05b724b018a8ed19b0a72df50
Roman Koshchei <romankoshchei@gmail.com>
2025-05-07 19:17:55 +0300

    backend

--- src/lib/server/api.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono';
-
-export const router = new Hono().basePath('/api').get('/', (c) => {
-	return c.text('Hello world!');
-});
-
-export type Router = typeof router;
--- src/routes/api/[...paths]/+server.ts
+++
@@ -1,6 +0,0 @@
-import { router } from '$lib/server/api';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const fallback: RequestHandler = async ({ request }) => {
-	return router.fetch(request);
-};
